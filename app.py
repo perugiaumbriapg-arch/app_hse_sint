179417188ce156b8ad7e305dc0996fa728a081c6
perugiaumbriapg-arch <perugia.umbria.pg@gmail.com>
2026-07-29 16:14:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2235,12 +2235,12 @@ if nav == "Piano Miglioramento":
                     st.rerun()
                 else:
                     st.error("Password errata.")
-        
+        else:
             # Caricamento e unione dati per dropdown
             opzioni = ["Nessuna (Nuova analisi)"]
                     
             # Leggi Near Miss
-            else os.path.exists(FILE_NEAR_MISS):
+            if os.path.exists(FILE_NEAR_MISS):
                 df_nm = pd.read_csv(FILE_NEAR_MISS, sep=";")
                 for idx, r in df_nm.iterrows():
                     opzioni.append(f"NM | {r.get('Data Segnalazione', 'N/D')} | {r.get('Tipo Evento', 'Evento')}")
